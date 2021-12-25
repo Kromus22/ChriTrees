3be03ee56f4b3296cefdaf0f8f6ffcc932cc3081
Kromus22 <kromus2@gmail.com>
2021-12-25 14:06:38 +0500

    +timer

---
+++ js/timer.js
@@ -0,0 +1,55 @@
+// склонение слов
+const declOfNum = (n, titles) => {
+  return n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+    0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+}
+
+const timer = () => {
+  const timer = document.createElement('div')
+  const timerText = document.createElement('p')
+  const timerCount = document.createElement('span')
+
+  timer.classList.add('timer')
+  timerText.classList.add('timer__text')
+  timerCount.classList.add('timer__count')
+
+  timerText.textContent = 'До конца распродажи осталось:'
+
+
+
+  timerText.append(timerCount)
+  timer.append(timerText)
+  document.body.prepend(timer)
+
+  const startTimer = () => {
+    //дата, ДО которой будем делать отсчёт. формат (год, месяц(0 - первый месяц, Январь) день, час, минута, секунда)
+    const deadline = new Date(2023, 0, 1, 0, 0, 0)
+    //текущее время / дата
+    const now = new Date()
+
+    const timeRemaining = (deadline - now) / 1000
+
+    //вычисляем количество оcтавшихся дней, часов и тд.
+    const seconds = Math.floor(timeRemaining % 60)
+    const minutes = Math.floor(timeRemaining / 60 % 60)
+    const hours = Math.floor(timeRemaining / 60 / 60 % 24)
+    const days = Math.floor(timeRemaining / 60 / 60 / 24)
+
+    const s = declOfNum(seconds, ['секунда', 'секунды', 'секунд'])
+    const m = declOfNum(minutes, ['минута', 'минуты', 'минут'])
+    const h = declOfNum(hours, ['час', 'часа', 'часов'])
+    const d = declOfNum(days, ['день', 'дня', 'дней'])
+
+    timerCount.textContent = `${d} ${h} ${m} ${s}`
+
+    if (timeRemaining > 0) {
+      setTimeout(startTimer, 1000)
+    } else {
+      timer.remove()
+    }
+  }
+
+  startTimer()
+}
+
+timer()
